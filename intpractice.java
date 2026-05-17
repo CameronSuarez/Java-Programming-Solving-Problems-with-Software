5d84f7290cafe9d72eb849bee2813112a915a50b
Cameron Suarez <Cameron.Suarez44@gmail.com>
2026-05-17 10:15:01 -0400

    Add files via upload

---
+++ intpractice.java
@@ -0,0 +1,8 @@
+public class intpractice {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = x + 2;
+        int z = y + x;
+        System.out.println(z);
+    }
+}
